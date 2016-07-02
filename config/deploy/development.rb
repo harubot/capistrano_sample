8aaa8a783da8cd30665d731e6466ba4ef8605306
ハルbot <harubot@users.noreply.github.com>
2016-07-02 20:53:06 +0900

    デプロイ先の環境は development ではなく staging

--- config/deploy/development.rb
+++
@@ -1,3 +0,0 @@
-# Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/var/www/capistrano_sample/capistrano_sample.development'
-
---
+++ config/deploy/staging.rb
@@ -0,0 +1,3 @@
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/var/www/capistrano_sample/capistrano_sample.staging'
+
